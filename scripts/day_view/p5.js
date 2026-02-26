d6cb25a5cb78579b4511e8f64c430460ad1751b4
TheGreedyGoat <alexander_schertl@teilnehmer.cdemy.de>
2026-02-26 16:47:01 +0100

    Der Pinnwand- Hintergrund ist jetzt performanter, da ein festes Bild genommen wird

--- scripts/day_view/p5.js
+++ scripts/day_view/p5.js
@@ -28,39 +28,60 @@ function buildBackground(p, parentid, pixelSize, variation, median, hue, sat){
     cnv.position(0, 0);
     cnv.style("z-index", "-2");
     p.colorMode(p.HSB, 360, 100, 100, 100)
-    p.noLoop();
-  } // end setup
-  
 
-  p.draw = function(){
-    p.loadPixels();
-
+    let img = p.createImage(50, 50);
+    img.loadPixels();
+    
     let dark = Math.min(median - variation)
     let bright = Math.min(median + variation)
 
-    for(let y = 0; y < p.height; y += pixelSize){
-      for(let x = 0; x < p.width; x += pixelSize){
-        
+    for(let y = 0; y < img.height; y += pixelSize){
+      for(let x = 0; x < img.width; x += pixelSize){
+
         let value = p.random(dark, bright);
         let c = p.color(hue, sat, value)
         let r = p.red(c)
         let g = p.green(c)
         let b = p.blue(c)
-    
+
         for(let dy = 0; dy < pixelSize; dy++){
           for(let dx = 0; dx < pixelSize; dx++){
-              let index = (pixelSize - 1) * ((x + dx) + (y + dy) * p.width)
-              
-              p.pixels[index ]    += r; //r
-              p.pixels[index + 1] += g; //g
-              p.pixels[index + 2] += b; //b
-              p.pixels[index + 3] += 255; //a
+            img.set(x + dx, y + dy, [r, g, b, 255]);
           }
         }
       }
     }
-    p.updatePixels()
-  }
+    img.updatePixels();
+    img.save("b_boardBackground.png")
+
+
+
+    p.noLoop();
+  } // end setup
+  
+
+  // // p.draw = function(){
+  // //   p.loadPixels();
+
+
+  // //   for(let y = 0; y < p.height; y += pixelSize){
+  // //     for(let x = 0; x < p.width; x += pixelSize){
+        
+    
+  // //       for(let dy = 0; dy < pixelSize; dy++){
+  // //         for(let dx = 0; dx < pixelSize; dx++){
+  // //             let index = (pixelSize - 1) * ((x + dx) + (y + dy) * p.width)
+              
+  // //             p.pixels[index ]    += r; //r
+  // //             p.pixels[index + 1] += g; //g
+  // //             p.pixels[index + 2] += b; //b
+  // //             p.pixels[index + 3] += 255; //a
+  // //         }
+  // //       }
+  // //     }
+  // //   }
+  // //   p.updatePixels()
+  // // }
 
   p.windowResized = function(){
     p.resizeCanvas(window.innerWidth, window.innerHeight)
